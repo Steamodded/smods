c95c0186531dc638b22768a268077f36d74f5659
Dorfyt <79335749+EnderDrag0n@users.noreply.github.com>
2025-04-26 15:50:18 +0300

    some fixes (thanks SpaD-Overolls)

--- localization/ru.lua
+++ localization/ru.lua
@@ -44,8 +44,8 @@ return {
             load_failure_p = {
                 text = {
                     '{C:attention}Конфликт префиксов!{}',
-                    'Префиксы этих модов',
-                    'такие же как у других.',
+                    'Префикс этого мода',
+                    'такой же как у другого.',
                     '({C:attention}#1#{})'
                 }
             },
@@ -68,7 +68,7 @@ return {
             -- card perma bonuses
             card_extra_chips={
                 text={
-                    "{C:chips}#1#{} экстра фишек",
+                    "{C:chips}#1#{} доп. фишек",
                 },
             },
             card_x_chips = {
@@ -78,27 +78,27 @@ return {
             },
             card_extra_x_chips = {
                 text = {
-                    "{X:chips,C:white}X#1#{} экстра фишек"
+                    "{X:chips,C:white}X#1#{} доп. фишек"
                 }
             },
             card_extra_mult = {
                 text = {
-                    "{C:mult}#1#{} экстра Модификатор"
+                    "{C:mult}#1#{} доп. Множ."
                 }
             },
             card_x_mult = {
                 text = {
-                    "{X:mult,C:white}X#1#{} Модификатор"
+                    "{X:mult,C:white}X#1#{} Множ."
                 }
             },
             card_extra_x_mult = {
                 text = {
-                    "{X:mult,C:white}X#1#{} экстра Модификатор"
+                    "{X:mult,C:white}X#1#{} доп. Множ."
                 }
             },
             card_extra_p_dollars = {
                 text = {
-                    "{C:money}#1#{} when scored",
+                    "{C:money}#1#{} когда засчитано",
                 }
             },
             card_extra_h_chips = {
@@ -113,22 +113,22 @@ return {
             },
             card_extra_h_x_chips = {
                 text = {
-                    "{X:chips,C:white}X#1#{} экстра фишек когда удерживается",
+                    "{X:chips,C:white}X#1#{} доп. фишек когда удерживается",
                 }
             },
             card_extra_h_mult = {
                 text = {
-                    "{C:mult}#1#{} экстра Модификатор когда удерживается",
+                    "{C:mult}#1#{} доп. Множ. когда удерживается",
                 }
             },
             card_h_x_mult = {
                 text = {
-                    "{X:mult,C:white}X#1#{} Модификатор когда удерживается",
+                    "{X:mult,C:white}X#1#{} Множ. когда удерживается",
                 }
             },
             card_extra_h_x_mult = {
                 text = {
-                    "{X:mult,C:white}X#1#{} экстра Модификатор когда удерживается",
+                    "{X:mult,C:white}X#1#{} доп. Множ. когда удерживается",
                 }
             },
             card_extra_h_dollars = {
@@ -139,7 +139,7 @@ return {
         },
         Edition = {
             e_negative_playing_card = {
-                name = "Негативный",
+                name = "Негативная",
                 text = {
                     "{C:dark_edition}+#1#{} размер руки"
                 },
@@ -149,22 +149,22 @@ return {
             m_gold = {
                 name = "Золотая карта",
                 text = {
-                    "Даёт {C:money}#1#{}, если",
-                    "удерживается в руке",
-                    "до конца раунда"
+                    "{C:money}$#1#{}, если эта",
+                    "карта есть в руке",
+                    "в конце раунда",
                 }
             },
             m_stone = {
                 name = "Каменная карта",
                 text = {
-                    "{C:chips}#1#{} фишек,",
-                    "не имеет достоинства и масти,", "всегда учитывается при подсчёте"
+                    "{C:chips}+#1#{} шт. фишек",
+                    "без достоинства или масти"
                 }
             },
             m_mult = {
                 name = "Карта с множителем",
                 text = {
-                    "{C:mult}#1#{} множ."
+                    "{C:mult}+#1#{} множ.",
                 }
             },
         }
@@ -181,7 +181,7 @@ return {
             b_mods_cap = 'МОДЫ',
             b_modded_version = 'Модифицированная версия!',
             b_steamodded = 'Steamodded',
-            b_credits = 'Титры',
+            b_credits = 'Авторы',
             b_open_mods_dir = 'Открыть папку с модами',
             b_no_mods = 'Модов не найдено...',
             b_mod_list = 'Список активированных модов',
@@ -189,17 +189,17 @@ return {
             b_developed_by = 'разработан ',
             b_rewrite_by = 'Переписан ',
             b_github_project = 'Github Проект',
-            b_github_bugs_1 = 'Вы можете сообщать о багах',
-            b_github_bugs_2 = 'и помочь в разработке здесь.',
+            b_github_bugs_1 = 'Вы можете сообщать нам о багах',
+            b_github_bugs_2 = 'и помочь в разработке.',
             b_disable_mod_badges = 'Отключить значки модов',
             b_author = 'Автор',
             b_authors = 'Авторы',
             b_unknown = 'Неизвестно',
-            b_lovely_mod = '(Lovely Mod) ',
+            b_lovely_mod = '(Мод на Lovely) ',
             b_by = ' От: ',
             b_priority = 'Приоритет: ',
 			b_config = "Настройки",
-			b_additions = 'Дополнения',
+			b_additions = 'Нововведения',
       		b_stickers = 'Наклейки',
 			b_achievements = "Достижения",
       		b_applies_stakes_1 = 'Применяет ',
@@ -209,10 +209,10 @@ return {
 			b_search_prompt = 'Поиск модов',
 			b_search_button = 'Поиск',
             b_seeded_unlocks = 'Разблокировка с сидами',
-            b_seeded_unlocks_info = 'Включает возможность открывать предметы коллекции в забегах с сидами',
+            b_seeded_unlocks_info = 'Включает возможность открывать предметы коллекции в партиях с сидами',
             ml_achievement_settings = {
-                'Отключёны',
-                'Включёны',
+                'Отключены',
+                'Включены',
                 'Обойти ограничения'
             },
             b_deckskins_lc = 'Низкоконтрастные цвета',
@@ -226,7 +226,7 @@ return {
             a_xchips_minus = "-X#1# фишек",
             smods_version_mismatch = {
                 "Версия Steamodded изменилась",
-                "с начала забега!",
+                "с начала партии!",
                 "Продолжение может привести к",
                 "неожиданным последствиям и крашам.",
                 "Изначальная версия: #1#",
