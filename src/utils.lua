f877c0dd24ab3dd7606188bf0082fac59b92ce18
Gloompashcy <gloompashcy@gmail.com>
2025-07-03 14:00:46 +1000

    SMODS.is_eternal

--- src/utils.lua
+++ src/utils.lua
@@ -2583,27 +2583,23 @@ G.FUNCS.update_blind_debuff_text = function(e)
     end
 end
 
-function SMODS.is_eternal(card, add_context)
-    add_context = add_context or {}
-    local eternal = {}
+function SMODS.is_eternal(card, trigger)
+    local calc_return = {}
+    local ovr_compat = false
     local ret = false
-    local context = {check_eternal = true, other_card = card, no_blueprint = true}
-    for k,v in pairs(add_context) do if not context[k] then context[k] = v end end
-    SMODS.calculate_context(context, eternal)
-    for _,tab in pairs(eternal) do
-        for k, val in pairs(tab) do
-            if type(val) == 'table' then
-                for key, val2 in pairs(val) do
-                    if key = 'eternal' and val2 then ret = true end
+    if not trigger then trigger = {} end
+    SMODS.calculate_context({check_eternal = true, other_card = card, trigger = trigger, no_blueprint = true,}, calc_return)
+    for _,eff in pairs(calc_return) do
+        for _,tab in pairs(eff) do
+            if tab.no_destroy then --Reuses key from context.joker_type_destroyed
+                ret = true
+                if type(tab.no_destroy) == 'table' then
+                    if tab.no_destroy.override_compat then ovr_compat = true end
                 end
-            elseif type(k) == 'string' and k == 'eternal' then
-                if val then ret = true end
             end
         end
     end
-    for k, _ in pairs(SMODS.get_enhancements(card)) do
-        if G.P_CENTERS[k].config and G.P_CENTERS[k].config.eternal then ret = true end
-    end
     if card.ability.eternal then ret = true end
+    if not card.config.center.eternal_compat and not ovr_compat then ret = false end
     return ret
 end
