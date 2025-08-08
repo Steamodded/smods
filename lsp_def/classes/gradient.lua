907d72ba18c22b1485fc44693a67d58e78f73ae4
Sebastián Venturino <venturinosebastian@gmail.com>
2025-08-08 11:02:05 -0300

    move update def

--- lsp_def/classes/gradient.lua
+++ lsp_def/classes/gradient.lua
@@ -4,6 +4,7 @@
 ---@field interpolation? 'trig'|'linear'|string Interpolation type of the gradient. Currently supported: `'trig'`, `'linear'`.
 ---@field colours? table<number, table<number, number>> List of colours to interpolate between.
 ---@field cycle? number Amount of time (in seconds) for the gradient to cycle through all colours.
+---@field update? fun(self: SMODS.Gradient|table, dt: number) Allows finer per-frame control over the implementation.
 ---@field __call? fun(self: SMODS.Gradient|table, o: SMODS.Gradient|table): nil|table|SMODS.Gradient
 ---@field extend? fun(self: SMODS.Gradient|table, o: SMODS.Gradient|table): table Primary method of creating a class. 
 ---@field check_duplicate_register? fun(self: SMODS.Gradient|table): boolean? Ensures objects already registered will not register. 
@@ -18,7 +19,6 @@
 ---@field inject? fun(self: SMODS.Gradient|table, i?: number) Called during `inject_class`. Injects the object into the game. 
 ---@field take_ownership? fun(self: SMODS.Gradient|table, key: string, obj: SMODS.Gradient|table, silent?: boolean): nil|table|SMODS.Gradient Takes control of vanilla objects. Child class must have get_obj for this to function
 ---@field get_obj? fun(self: SMODS.Gradient|table, key: string): SMODS.Gradient|table? Returns an object if one matches the `key`.
----@field update? fun(self: SMODS.Gradient|table, dt: number) Allows finer per-frame control over the implementation.
 ---@overload fun(self: SMODS.Gradient): SMODS.Gradient
 SMODS.Gradient = setmetatable({}, {
     __call = function(self)
