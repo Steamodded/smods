5a69507c1e2e2b59f535358b04da834d6e791607
cg223 <cg223celeste@gmail.com>
2025-10-06 23:54:48 -0400

    Add proper error handling for apply_patches failure

--- lsp_def/lib/lovely.lua
+++ lsp_def/lib/lovely.lua
@@ -5,8 +5,9 @@ local lovely = {}
 ---@type function
 ---@param target string
 ---@param buffer string
+---@return string | boolean
 ---@return string
---- Lovely version >= 0.9.0: Applies patches for `target` on `buffer`, returns patched buffer.
+--- Lovely version >= 0.9.0: Applies patches for `target` on `buffer`, returns patched buffer. Can fail, returning `false` and an error message.
 lovely.apply_patches = function(target, buffer) end
 
 ---@type string
