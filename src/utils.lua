5443e1d4fe466bff16798240f9c4f2faecfe5fdc
the-Astra <djs8743@rit.edu>
2025-12-28 16:50:23 -0500

    Update utils.lua

--- src/utils.lua
+++ src/utils.lua
@@ -3410,3 +3410,13 @@ function SMODS.log_crash_info(info, defined)
         return string.format("\n\nError exists in %s at line %d\r\n", info.source, line)
     end
 end
+
+function SMODS.get_clean_pool(_type, _rarity, _legendary, _append)
+    local pool = get_current_pool(_type, _rarity, _legendary, _append)
+    for i, v in ipairs(pool) do
+        if v == 'UNAVAILABLE' then
+            pool[i] = nil
+        end
+    end
+    return pool
+end
