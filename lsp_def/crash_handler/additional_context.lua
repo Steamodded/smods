b2281d11755551d3bbe968cd38f5d4665c86fdf6
frostice482 <frostice48260@gmail.com>
2026-03-07 18:13:33 +0700

    lsp

---
+++ lsp_def/crash_handler/additional_context.lua
@@ -0,0 +1,5 @@
+--- @meta SMODS.crash_handler.additional_context
+
+--- @type SMODS.CrashHandler.AdditionalContext
+local x
+return x
---
+++ lsp_def/crash_handler/stack_format.lua
@@ -0,0 +1,5 @@
+--- @meta SMODS.crash_handler.stack_format
+
+--- @type SMODS.CrashHandler.StackTraceFormatter
+local x
+return x
