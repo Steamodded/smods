c87bf71ac2e2800decc1149b43d1ab20451d848e
makatymba2001 <makatymba2001@yandex.ru>
2026-03-07 03:35:48 +0200

    remove unused code

--- src/utils.lua
+++ src/utils.lua
@@ -485,18 +485,9 @@ function SMODS.create_mod_badges(obj, badges)
         for i, mod in ipairs(mods) do
             local mod_name = mod.display_name
             local size = 0.9
-            local font = G.LANG.font
             local max_text_width = 2 - 2*0.05 - 4*0.03*size - 2*0.03
-            -- local calced_text_width = 0
-            -- -- Math reproduced from DynaText:update_text
-            -- for _, c in utf8.chars(mod_name) do
-            --     local tx = font.FONT:getWidth(c)*(0.33*size)*G.TILESCALE*font.FONTSCALE + 2.7*1*G.TILESCALE*font.FONTSCALE
-            --     calced_text_width = calced_text_width + tx/(G.TILESIZE*G.TILESCALE)
-            -- end
             local scale_fac = 1
             local badge_text = DynaText({string = mod_name or 'ERROR', colours = {mod.badge_text_colour or G.C.WHITE},float = true, shadow = true, offset_y = -0.05, silent = true, spacing = 1*scale_fac, scale = 0.33*size*scale_fac})
-                -- calced_text_width > max_text_width and max_text_width/calced_text_width
-                -- or 1
             badges[#badges + 1] = {n=G.UIT.R, config={align = "cm"}, nodes={
                 {n=G.UIT.R, config={align = "cm", colour = mod.badge_colour or G.C.GREEN, r = 0.1, minw = 2, minh = 0.36, emboss = 0.05, padding = 0.03*size}, nodes={
                   {n=G.UIT.B, config={h=0.1,w=0.03}},
