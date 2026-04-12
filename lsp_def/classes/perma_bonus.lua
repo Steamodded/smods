e1a5daedee61940579293fd618fc03efdb4a7727
Gloompashcy <gloompashcy@gmail.com>
2026-04-12 21:19:58 +1000

    vars_key should be a string

--- lsp_def/classes/perma_bonus.lua
+++ lsp_def/classes/perma_bonus.lua
@@ -24,7 +24,7 @@
 ---@field get_ui_value? fun(self: SMODS.PermaBonus|table, card: Card|table): number|nil Returns the number to be used for the display in the ui box, returns `nil` if the bonus is 0. 
 ---@field upgrade? fun(self: SMODS.PermaBonus|table, card: Card|table, amount: number?, from?: Card|table?) Called through SMODS.upgrade_perma_bonus, allows for control over how the perma bonus is upgraded. 
 ---@field loc_key? string The key of the object in G.localization.descriptions.Other, defaults to the object's key. 
----@field vars_keys? table How the value of the bonus is stored in the specific_vars table for playing card ui, defaults to the object's key. 
+---@field vars_key? string How the value of the bonus is stored in the specific_vars table for playing card ui, defaults to the object's key. 
 ---@field signed_value? boolean If this is `true`, the value in the ui box will be signed with `SMODS.signed`. 
 ---@field signed_dollars? boolean Same as signed_value except the value is signed using `SMODS.signed_dollars`, has priority over signed_value by deafult. 
 ---@field localize? fun(self: SMODS.PermaBonus|table, value: number, desc_nodes: table) Defines what to show in the playing card description, not recommended to change unless you know what you're doing. 
