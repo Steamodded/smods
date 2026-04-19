699e8751a514d909f1afed1549453d9b8aaa8531
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-04-20 02:41:38 +0800

    no longer adding an additional parameter, instead using a global

--- src/utils.lua
+++ src/utils.lua
@@ -412,7 +412,8 @@ function SMODS.create_card(t)
     SMODS.bypass_create_card_discovery_center = t.bypass_discovery_center
     SMODS.set_create_card_front = G.P_CARDS[t.front]
     SMODS.create_card_allow_duplicates = t.allow_duplicates
-    local _card = create_card(t.set, t.area, t.legendary, t.rarity, t.skip_materialize, t.soulable, t.key, t.key_append, t.scale)
+    SMODS.create_card_scale = t.scale or {w = 1, h = 1}
+    local _card = create_card(t.set, t.area, t.legendary, t.rarity, t.skip_materialize, t.soulable, t.key, t.key_append)
     SMODS.bypass_create_card_edition = nil
     SMODS.bypass_create_card_discover = nil
     SMODS.bypass_create_card_discovery_center = nil
