a38a2425961b0b97277f4b7e24f43064c3ec9de6
AllUniversal <alluniversal@gmx.ch>
2026-05-07 22:11:34 +0200

    Removed some Blind related code
    -Title

--- src/game_objects/ante_path.lua
+++ src/game_objects/ante_path.lua
@@ -75,7 +75,7 @@ SMODS.AntePath = SMODS.GameObject:extend {
                         {key = "next_ante_path", triggers = {cashed_out = true}},
                         {key = "enter_shop", triggers = {cashed_out = true}},
                     },
-                    blinds = {SMODS.get_new_blind({[boss_type] = true})},
+                    blinds = {SMODS.get_new_blind({[boss_type] = true})}, -- Todo : Replace with correct function
                 }
             }
         }
@@ -495,104 +495,6 @@ Game:start_run() -> get_next_tag_key() is called, besides that; blind_states is 
 ]]
 function reset_blinds()
     G.GAME.round_resets.boss_rerolled = false
-end
-
-function SMODS.get_blind_types(blind_obj)
-    if type(blind_obj.get_types) == "function" then
-        return blind_obj:get_types()
-    else
-        if v.boss then
-            if not v.boss.showdown then
-                return {Boss = true}
-            else
-                return {Showdown = true}
-            end
-        else
-            if v.name == "Small Blind" then
-                return {Small = true}
-            else
-                return {Big = true}
-            end
-        end
-    end
-end
-
-function get_new_boss()
-    sendWarnMessage("get_new_boss() is deprecated; Call SMODS.get_new_blind() instead.", "utils")
-end
-
-function SMODS.get_new_blind(blind_types)
-    if not blind_types or (blind_types.Boss or blind_types.Showdown) then
-        G.GAME.perscribed_bosses = G.GAME.perscribed_bosses or {}
-        if G.GAME.perscribed_bosses and G.GAME.perscribed_bosses[G.GAME.round_resets.ante] then
-            local ret_boss = G.GAME.perscribed_bosses[G.GAME.round_resets.ante] 
-            G.GAME.perscribed_bosses[G.GAME.round_resets.ante] = nil
-            G.GAME.bosses_used[ret_boss] = G.GAME.bosses_used[ret_boss] + 1
-            return ret_boss
-        end
-        if G.FORCE_BOSS then return G.FORCE_BOSS end
-    end
-
-    local eligible_bosses = {}
-    for k, v in pairs(G.P_BLINDS) do
-        local res = SMODS.add_to_pool(v)
-        if res then
-            local b_types = SMODS.get_blind_types(v)
-            for _, b_type in pairs(b_types) do
-                if blind_types[b_type] then
-                    if v.in_pool or not v.boss or (v.boss.min <= math.max(1, G.GAME.round_resets.ante) and ((math.max(1, G.GAME.round_resets.ante))%G.GAME.win_ante ~= 0 or G.GAME.round_resets.ante < 2)) then
-                        eligible_bosses[k] = true
-                        break
-                    end
-                end
-            end
-        end
-    end
-    for k, v in pairs(G.GAME.banned_keys) do
-        if eligible_bosses[k] then eligible_bosses[k] = nil end
-    end
-
-    local min_use = 100
-    for k, v in pairs(G.GAME.bosses_used) do
-        if eligible_bosses[k] then
-            eligible_bosses[k] = v
-            if eligible_bosses[k] <= min_use then 
-                min_use = eligible_bosses[k]
-            end
-        end
-    end
-    for k, v in pairs(eligible_bosses) do
-        if eligible_bosses[k] then
-            if eligible_bosses[k] > min_use then 
-                eligible_bosses[k] = nil
-            end
-        end
-    end
-    local _, boss = pseudorandom_element(eligible_bosses, pseudoseed('boss'))
-    G.GAME.bosses_used[boss] = G.GAME.bosses_used[boss] + 1
-    
-    return boss
-end
-
-function Blind:get_type()
-    return SMODS.get_blind_types(self.config.blind)
-end
-
-function Blind:is_type(b_type)
-    return self:is_types({[b_type] = true}, false)
-end
-
-function Blind:is_types(b_types_map, all)
-    for k, v in pairs(self:get_type()) do
-        if v and b_types_map[k] or (k == "Showdown" and b_types_map.Boss) then
-            if not all then
-                return true
-            end
-        elseif all then
-            return false
-        end
-    end
-    return all
 end
 
 SMODS.Joker:take_ownership("j_matador", {
