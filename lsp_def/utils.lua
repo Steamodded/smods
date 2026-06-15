038a1c593537292dbec222f4c61faaf8ca00e697
AllUniversal <alluniversal@gmx.ch>
2026-06-15 23:08:57 +0200

    Refactored out unnecessary setting of `T.w/h` in favor of just `T.scale`
    */-Title, this also fixes Seals/etc. rendering at the wrong size.

--- lsp_def/utils.lua
+++ lsp_def/utils.lua
@@ -892,6 +892,6 @@ function SMODS.add_to_deck(card, args) end
 
 -- Util function to render one card to a `.png` file, saved to `love.filesystem.getSaveDirectory()`
 ---@param card Card|table Card to save as an image
----@param scale number? Scale to render the card at (default = 2.0)
+---@param scale number? Scale to render the card at (default = G.SETTINGS.GRAPHICS.texture_scaling)
 ---@param filename string? Name of the file (default = [center.key])
 function SMODS.card_to_image(card, scale, filename) end
