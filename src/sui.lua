8e61fa665d18389ac63e2a846abe814bd59f6d26
SleepyG11 <makatymba2001@yandex.ru>
2026-06-30 12:59:27 +0300

    Renamings, SUI.T, optimizations, comments, topology adjustments

--- src/sui.lua
+++ src/sui.lua
@@ -14,9 +14,12 @@ SMODS.SUI.special_config_keys = {
 
 -- Utilities
 
+-- Formats number from range 0-1 to HEX component 00-FF
 function SMODS.SUI.to_hex(v)
 	return string.format("%02X", math.floor(v * 255 + 0.5))
 end
+-- Formats colour from format `{ r, g, b, a }` to `#RRGGBB`.
+-- If `a` is less than 1, format to `#RRGGBBAA` instead.
 function SMODS.SUI.colour_to_hex(colour)
 	colour = colour or { 0, 0, 0, 0 }
 
@@ -28,18 +31,20 @@ function SMODS.SUI.colour_to_hex(colour)
 
 	return hex
 end
-function SMODS.SUI.input_type(i)
-	local itype = type(i)
+-- Extended Lua's `type()` function to check is passed input is `Moveable` or can be treated as node definition.
+function SMODS.SUI.input_type(input)
+	local itype = type(input)
 	if itype == "table" then
-		if Object.is(i, Moveable) then
+		if Object.is(input, Moveable) then
 			return "moveable"
 		end
-		if i.n then
+		if input.n then
 			return "node"
 		end
 	end
 	return itype or "nil"
 end
+-- Easy way to attach multiple hooks to table in bulk. In hook function passed as first argument, all other arguments goes after it.
 function SMODS.SUI.attach_hooks(target, hooks)
 	if not target or not hooks then
 		return target
@@ -52,6 +57,7 @@ function SMODS.SUI.attach_hooks(target, hooks)
 	end
 	return target
 end
+-- Checks is element's type considered as column element.
 function SMODS.SUI.is_column_type(n)
 	return n == G.UIT.C or n == G.UIT.B or n == G.UIT.T or n == G.UIT.O
 end
@@ -61,7 +67,7 @@ end
 SMODS.SUI.UIT_STORAGE = {
 	buffer = {},
 	fallback_to_unknown = false,
-	get_tables = function()
+	get_tables = function(self)
 		return {
 			[""] = G.UIT,
 		}
@@ -72,7 +78,7 @@ SMODS.SUI.DEBUG_UIT_STORAGE = {
 	fallback_to_unknown = true,
 	unknown_next_index = 1,
 	unknown_prefix = "?UIT",
-	get_tables = function()
+	get_tables = function(self)
 		return {
 			[""] = G.UIT,
 		}
@@ -83,7 +89,7 @@ SMODS.SUI.DEBUG_CLASS_STORAGE = {
 	fallback_to_unknown = true,
 	unknown_next_index = 1,
 	unknown_prefix = "?Class",
-	get_tables = function()
+	get_tables = function(self)
 		return {
 			[""] = _G,
 			["SMODS."] = SMODS,
@@ -99,10 +105,10 @@ function SMODS.SUI.create_storage(storage)
 			return storage.buffer[input]
 		end
 		-- Process all tables looking for input
-		for _k, _v in pairs(storage.get_tables()) do
-			for k, v in pairs(_v) do
-				if v == input then
-					local result_key = _k .. k
+		for table_key, table_value in pairs(storage:get_tables()) do
+			for key, value in pairs(table_value) do
+				if value == input then
+					local result_key = table_key .. key
 					storage.buffer[input] = result_key
 					return result_key
 				end
@@ -133,32 +139,37 @@ SMODS.SUI.get_debug_object_class_name = SMODS.SUI.create_storage(SMODS.SUI.DEBUG
 function SMODS.SUI.config_merge(node, key, value)
 	node.config[key] = value
 end
-function SMODS.SUI.node_merge(node, index, value)
-	node.nodes[table.maxn(node.nodes) + 1] = value
+function SMODS.SUI.node_merge(node, index, child)
+	node.nodes[table.maxn(node.nodes) + 1] = child
 end
-function SMODS.SUI.extended_process_node(node, index, v)
-	local input_type = SMODS.SUI.input_type(v)
+function SMODS.SUI.extended_process_node(node, index, child)
+	local input_type = SMODS.SUI.input_type(child)
+
+	-- Wrap Moveable into G.UIT.O
 	if input_type == "moveable" then
-		v = SUI.O({ v })
+		child = SUI.O({ child })
 		input_type = "node"
+	-- Wrap string and numbers in G.UIT.T
 	elseif input_type == "string" or input_type == "number" then
-		v = SUI.T({ colour = G.C.UI.TEXT_LIGHT, scale = 0.32, v })
+		child = SUI.T({ colour = G.C.UI.TEXT_LIGHT, scale = 0.32, text = child })
 		input_type = "node"
 	end
 	if input_type == "node" then
-		if node.s_config.rc_fix_target == nil then
-			if SMODS.SUI.is_column_type(v.n) then
+		-- Perform R/C fix: element behave unpredictably when R/C elements are mixed inside one parent.
+		-- if target not specified, first element's type will used as target.
+		if node.s_config.rc_fix_target == nil or node.s_config.rc_fix_target == true then
+			if SMODS.SUI.is_column_type(child.n) then
 				node.s_config.rc_fix_target = "C"
-			elseif v.n == G.UIT.R then
+			elseif child.n == G.UIT.R then
 				node.s_config.rc_fix_target = "R"
 			end
-			SMODS.SUI.Node.process_node(node, index, v)
-		elseif node.s_config.rc_fix_target == "C" and not SMODS.SUI.is_column_type(v.n) then
-			SMODS.SUI.Node.process_node(node, index, SUI.C({ v }))
-		elseif node.s_config.rc_fix_target == "R'" and SMODS.SUI.is_column_type(v.n) then
-			SMODS.SUI.Node.process_node(node, index, SUI.R({ v }))
+			SMODS.SUI.Node.process_node(node, index, child)
+		elseif node.s_config.rc_fix_target == "C" and not SMODS.SUI.is_column_type(child.n) then
+			SMODS.SUI.Node.process_node(node, index, SUI.C({ align = node.s_config.rc_fix_align, child }))
+		elseif node.s_config.rc_fix_target == "R" and SMODS.SUI.is_column_type(child.n) then
+			SMODS.SUI.Node.process_node(node, index, SUI.R({ align = node.s_config.rc_fix_align, child }))
 		else
-			SMODS.SUI.Node.process_node(node, index, v)
+			SMODS.SUI.Node.process_node(node, index, child)
 		end
 	end
 end
@@ -166,9 +177,11 @@ function SMODS.SUI.process_node_render(node)
 	return node.render and node:render() or node
 end
 function SMODS.SUI.process_element_created(element)
-	SMODS.SUI.attach_hooks(element, element.input_node.s_hooks)
-	if element.input_node.s_init then
-		element.input_node.s_init(element)
+	if element.input_node then
+		SMODS.SUI.attach_hooks(element, element.input_node.s_hooks)
+		if element.input_node.s_init then
+			element.input_node.s_init(element)
+		end
 	end
 end
 
@@ -177,11 +190,11 @@ end
 function SMODS.SUI.get_topology(node, options, indent)
 	options = options or {}
 	indent = indent or ""
-	if node.render then
-		node = node:render()
-	end
-	local formatted_class_name = ""
+	node = SMODS.SUI.process_node_render(node)
 	node.config = node.config or {}
+
+	-- Resolve class name & id and format element in tree as `n#id.class1.class2`
+	local formatted_class_name = ""
 	if node.s_config and node.s_config.class then
 		for word in string.gmatch(node.s_config.class, "%S+") do
 			formatted_class_name = formatted_class_name .. "." .. word
@@ -191,22 +204,29 @@ function SMODS.SUI.get_topology(node, options, indent)
 	if node.config.id then
 		formatted_id = "#" .. node.config.id
 	end
+
+	-- Resolve element topology info
 	local topology_info
 	if not node.topology then
 		local uit = SMODS.SUI.get_debug_uit_key(node.n)
 		topology_info = (SUI[uit] or SMODS.SUI.Node).topology(node)
 	else
-		topology_info = node:topology()
+		topology_info = node:topology(options)
 	end
+
 	local formatted_n = topology_info.n
 	local element_line = string.format("%s<%s%s%s", indent, formatted_n, formatted_id, formatted_class_name)
+
+	-- Resolve attributes to display and format them as `attr_1=value_1 attr_2=value_2`
 	local additional_attributes = ""
-	for _, v in pairs(topology_info.attributes or {}) do
-		additional_attributes = additional_attributes .. v .. " "
+	for k, v in pairs(topology_info.attributes or {}) do
+		additional_attributes = additional_attributes .. k .. "=" .. v .. " "
 	end
 	if additional_attributes ~= "" then
 		element_line = element_line .. " " .. additional_attributes
 	end
+
+	-- Format children if present
 	local with_children = false
 	for _, subnode in pairs(topology_info.nodes or {}) do
 		if not with_children then
@@ -215,11 +235,14 @@ function SMODS.SUI.get_topology(node, options, indent)
 		end
 		element_line = element_line .. "\n" .. SMODS.SUI.get_topology(subnode, options, indent .. "    ")
 	end
+
+	-- Close element
 	if with_children then
 		element_line = element_line .. "\n" .. string.format("%s</%s>", indent or "", formatted_n)
 	else
 		element_line = element_line .. " />"
 	end
+
 	return element_line
 end
 function SMODS.SUI.print_topology(node, options)
@@ -271,13 +294,12 @@ function SMODS.SUI.Node:process_node(index, value)
 end
 function SMODS.SUI.Node:pre_process_input(input)
 	local input_type = SMODS.SUI.input_type(input)
-	if input_type ~= "table" and input_type ~= "nil" then
-		input = { input }
-		input_type = "table"
-	end
 	if input_type == "table" then
 		return input
+	elseif input_type ~= "nil" then
+		return { input }
 	end
+	return nil
 end
 function SMODS.SUI.Node:post_process_input(input)
 	if input.s_config then
@@ -338,6 +360,7 @@ function SMODS.SUI.Node:process_inputs(...)
 end
 
 function SMODS.SUI.Node:render()
+	-- Propagate CSS stylesheet element uses
 	if self.s_config.css ~= nil then
 		for _, child in ipairs(self.nodes) do
 			if not child.s_config then
@@ -348,22 +371,32 @@ function SMODS.SUI.Node:render()
 			end
 		end
 	end
+
+	-- Apply CSS classes
 	if self.s_config.class then
-		local classes = self.s_config.css or SMODS.SUI.CSS or {}
-		local classname = self.s_config.class
+		local classes = self.s_config.css or SMODS.SUI.CSS
 		local old_config = self.config
+		local at_least_one_match = false
 		self.config = {}
-		for word in string.gmatch(classname, "%S+") do
+
+		for word in string.gmatch(self.s_config.class, "%S+") do
 			if classes[word] then
+				at_least_one_match = true
 				for k, v in pairs(classes[word]) do
 					self:process_config(k, v)
 				end
 			end
 		end
-		for k, v in pairs(old_config) do
-			self:process_config(k, v)
+
+		if at_least_one_match then
+			for k, v in pairs(old_config) do
+				self:process_config(k, v)
+			end
+		else
+			self.config = old_config
 		end
 	end
+
 	return {
 		n = self.n,
 		config = self.config,
@@ -375,7 +408,7 @@ function SMODS.SUI.Node:render()
 		s_init = self.s_init,
 	}
 end
-function SMODS.SUI.Node:topology()
+function SMODS.SUI.Node:topology(options)
 	self.config = self.config or {}
 	return {
 		n = SMODS.SUI.get_debug_uit_key(self.n),
@@ -385,14 +418,14 @@ function SMODS.SUI.Node:topology()
 		s_config = self.s_config or {},
 
 		attributes = {
-			align = self.config.align and string.format('align="%s"', self.config.align),
-			colour = self.config.colour and string.format("colour=%s", SMODS.SUI.colour_to_hex(self.config.colour)),
-			w = self.config.w and string.format("w=%.2f", self.config.w),
-			minw = self.config.minw and string.format("minw=%.2f", self.config.minw),
-			maxw = self.config.maxw and string.format("maxw=%.2f", self.config.maxw),
-			h = self.config.w and string.format("h=%.2f", self.config.h),
-			minh = self.config.minh and string.format("minh=%.2f", self.config.minh),
-			maxh = self.config.maxh and string.format("maxh=%.2f", self.config.maxh),
+			align = self.config.align and string.format('"%s"', self.config.align),
+			colour = self.config.colour and string.format("%s", SMODS.SUI.colour_to_hex(self.config.colour)),
+			w = self.config.w and string.format("%.2f", self.config.w),
+			minw = self.config.minw and string.format("%.2f", self.config.minw),
+			maxw = self.config.maxw and string.format("%.2f", self.config.maxw),
+			h = self.config.w and string.format("%.2f", self.config.h),
+			minh = self.config.minh and string.format("%.2f", self.config.minh),
+			maxh = self.config.maxh and string.format("%.2f", self.config.maxh),
 		},
 	}
 end
@@ -410,17 +443,16 @@ end
 SUI = setmetatable({
 	padding = G.UIT.padding,
 }, {
-	__call = function(t, ...)
+	__call = function(sui, ...)
 		local args = { ... }
 		local uit
 
 		local arg = args[1]
 		if type(arg) == "table" then
+			-- Try to resolve arg.n as n
 			uit = arg.n and (G.UIT[arg.n] and arg.n or SMODS.SUI.get_uit_key(arg.n)) or nil
-			if uit then
-				arg.n = nil
-			end
 		elseif arg then
+			-- Try to resolve arg itself as n
 			uit = G.UIT[arg] and arg or SMODS.SUI.get_uit_key(arg)
 			if uit then
 				table.remove(args, 1)
@@ -428,66 +460,61 @@ SUI = setmetatable({
 		end
 
 		if uit then
-			return t[uit](unpack(args))
+			return sui[uit](unpack(args))
 		end
 		error("Not found node type for SMODS.SUI node")
 	end,
-	__index = function(t, k)
-		if G.UIT[k] then
-			t[k] = SMODS.SUI.extend_uit(G.UIT[k])
-			return t[k]
+	__index = function(sui, n)
+		if G.UIT[n] then
+			sui[n] = SMODS.SUI.extend_uit(G.UIT[n])
+			return sui[n]
 		end
-		error("Not found node type for SMODS.SUI node: " .. tostring(k))
+		error("Not found node type for SMODS.SUI node: " .. tostring(n))
 	end,
 })
 
 -- Vanilla node elements and their extensions
 
--- G.UIT.B - just a box without any child elements
+-- `G.UIT.B` - just a box without any child elements
 SUI.B = SMODS.SUI.extend_uit(G.UIT.B, {
-	process_node = function(self, node, index, v) end,
+	process_node = function(self, index, child) end,
 })
 
--- G.UIT.ROOT - starting element of each UIBox
+-- `G.UIT.ROOT` - starting element of each UIBox
 -- Fixes R/C inside, covers text into T, objects into O
 SUI.ROOT = SMODS.SUI.extend_uit(G.UIT.ROOT, {
 	process_node = SMODS.SUI.extended_process_node,
 })
 
--- G.UIT.C - column element
+-- `G.UIT.C` - column element
 -- Fixes R/C inside, covers text into T, objects into O
 SUI.C = SMODS.SUI.extend_uit(G.UIT.C, {
 	process_node = SMODS.SUI.extended_process_node,
 })
 
--- G.UIT.R - row element
+-- `G.UIT.R` - row element
 -- Fixes R/C inside, covers text into T, objects into O
 SUI.R = SMODS.SUI.extend_uit(G.UIT.R, {
 	process_node = SMODS.SUI.extended_process_node,
 })
 
--- G.UIT.O - object element
--- can have only 1 child - object to render; all other removes old and place new
+-- `G.UIT.O` - object element
+-- Can have only 1 child - object to render; all other removes old and place new
 SUI.O = SMODS.SUI.extend_uit(G.UIT.O, {
-	process_node = function(self, index, v)
+	process_node = function(self, index, child)
 		if self.config.object then
 			self.config.object:remove()
 		end
-		self.config.object = v
+		self.config.object = child
 	end,
-	topology = function(self)
-		local old_topology = SMODS.SUI.Node.topology(self)
+	topology = function(self, options)
+		local old_topology = SMODS.SUI.Node.topology(self, options)
 		local meta = self.config.object and getmetatable(self.config.object)
-		old_topology.attributes.object = "object={"
-			.. (meta and SMODS.SUI.get_debug_object_class_name(meta) or "nil")
-			.. "}"
+		old_topology.attributes.object = "{" .. (meta and SMODS.SUI.get_debug_object_class_name(meta) or "nil") .. "}"
 		return old_topology
 	end,
 	render = function(self)
 		local r = SMODS.SUI.Node.render(self)
-		if type(r.config.object_func) == "function" and not r.config.object then
-			r.config.object = r.config.object_func(self)
-		end
 		if not r.config.object then
 			r.config.object = Moveable()
 		end
@@ -495,23 +522,41 @@ SUI.O = SMODS.SUI.extend_uit(G.UIT.O, {
 	end,
 })
 
+-- `G.UIT.T` - text element
+-- Can have only 1 child - text to render; replaces old text with new one
+SUI.T = SMODS.SUI.extend_uit(G.UIT.T, {
+	process_node = function(self, index, child)
+		self.config.text = child
+	end,
+	topology = function(self, options)
+		local old_topology = SMODS.SUI.Node.topology(self, options)
+		old_topology.attributes.text = self.config.text
+		if self.config.ref_table and self.config.ref_value then
+			old_topology.attributes.text = tostring(self.config.ref_table[self.config.ref_value])
+		end
+		old_topology.attributes.text = old_topology.attributes.text or "[UI ERROR]"
+		old_topology.attributes.scale = self.config.scale and string.format("%.2f", self.config.scale) or "nil"
+		return old_topology
+	end,
+})
+
 -- Useful shortcuts for vanilla elements
 
--- SUI.ROOT with default colour G.C.CLEAR instead of G.C.BLACK
+-- `SUI.ROOT` with default colour `G.C.CLEAR` instead of `G.C.BLACK`
 SUI.CLEAR_ROOT = SUI.ROOT:extend({
 	setup = function(self, ...)
 		SUI.ROOT.setup(self, ...)
 		self:process_input({ colour = G.C.CLEAR })
 	end,
 })
--- SUI.C with default align = "cm"
+-- `SUI.C` with default `align = "cm"`
 SUI.CENTER_C = SUI.C:extend({
 	setup = function(self, ...)
 		SUI.C.setup(self, ...)
 		self:process_input({ align = "cm" })
 	end,
 })
--- SUI.R with default align = "cm"
+-- `SUI.R` with default `align = "cm"`
 SUI.CENTER_R = SUI.R:extend({
 	setup = function(self, ...)
 		SUI.R.setup(self, ...)
