56780cb0075decb73f9a0fbf7a2a0665d4b8feaf
Ruby <138878489+lord-ruby@users.noreply.github.com>
2026-08-16 15:48:34 +0100

    fix crash again again

--- src/overrides.lua
+++ src/overrides.lua
@@ -3101,8 +3101,7 @@ function _G.create_UIBox_your_collection_decks()
     SMODS.RunSelect.Functions.build_selection_areas(key)
 
 
-    local page_ui = page_def.definition or
-    page_def.pool and function()
+    local page_ui = function()
         return {n=G.UIT.C, config = {padding = 0.1}, nodes ={
                 SMODS.RunSelect.Functions.build_selection_ui(key, true),
                 SMODS.RunSelect.Functions.create_page_cycle(key, page_def.amount)
